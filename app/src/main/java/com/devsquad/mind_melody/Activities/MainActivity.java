7ab6534dae45cfc9bc54b489afc6cacd688676d3
Calvin Li <jiaxili1115@163.com>
2024-10-14 14:46:31 +1100

    added login layout

--- app/src/main/java/com/devsquad/mind_melody/Activities/MainActivity.java
+++ app/src/main/java/com/devsquad/mind_melody/Activities/MainActivity.java
@@ -10,17 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.devsquad.mind_melody.R;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // 加载 activity_main.xml 布局
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
